b3a9c788d42e85fe0940b8dc50836476b4c6b5da
Pascal Fares <pfares@cofares.net>
2016-03-18 09:26:15 +0200

    resources....

--- Acceuil.php
+++ Acceuil.php
@@ -2,8 +2,8 @@
     include "PhpInc/autochanel.inc";
     $crc=new ChanelRow;
     $crc->addRow();
-    $crc->addChanel("Portail des applications de l'ISSAE Cnam Liban", "chan/annonces.html",1);
-    $crc->addChanel("Six raisons de vous former au CNAM LIBAN","chan/six.html",1);
+    $crc->addChanel("Portail des applications", "chan/annonces.html",1);
+    $crc->addChanel("Vous former au Cnam Liban","chan/six.html",1);
     $crc->addRow();
     $crc->addChanel("L'équipe Recherche et Développement", "chan/actualite.html",2);
     $crc->gen();
